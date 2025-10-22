6fe1c65723b2bc8db9647971ccfa67c2be2a4aea
Dan Richmond <40435281+asddataking@users.noreply.github.com>
2025-10-22 15:43:56 -0400

    Debug

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -190,11 +190,10 @@ export default function ShopPage() {
                 </div>
                 
                 <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-                  {filteredProducts.map((product, index) => (
+                  {filteredProducts.map((product) => (
                     <CardProduct
                       key={product.id}
                       product={product}
-                      index={index}
                     />
                   ))}
                 </div>
